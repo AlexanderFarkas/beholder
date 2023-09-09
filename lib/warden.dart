537d276b6e187a1ed8bcc6d15ea59cf7b1881967
AlexanderFarkas <sashafarkas322@gmail.com>
2023-09-09 13:40:36 +0300

    missing export

--- lib/warden.dart
+++ lib/warden.dart
@@ -4,6 +4,7 @@ export 'src/core.dart';
 export 'src/computed.dart';
 export 'src/future.dart';
 export 'src/view_model.dart';
+export 'src/proxy_observable_mixin.dart';
 export 'src/observer.dart';
 export 'src/form.dart';
 export 'src/typedefs.dart';
